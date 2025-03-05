a1b9f8ecb9ee40c613e7d7a386239e70529c95b7
gyorutan <waou1998@gmail.com>
2025-03-06 06:00:39 +0900

    update

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -9,11 +9,9 @@ import interactionPlugin from "@fullcalendar/interaction";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 
 import React, { useState } from "react";
